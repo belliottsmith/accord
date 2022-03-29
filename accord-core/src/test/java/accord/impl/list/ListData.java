6fca6a7199de1897da10813a0950fc908ece3ee6
Blake Eggleston <bdeggleston@gmail.com>
2022-03-29 10:13:29 -0700

    add ListData.toString

--- accord-core/src/test/java/accord/impl/list/ListData.java
+++ accord-core/src/test/java/accord/impl/list/ListData.java
@@ -1,5 +1,7 @@
 package accord.impl.list;
 
+import java.util.Arrays;
+import java.util.Map;
 import java.util.TreeMap;
 
 import accord.api.Data;
@@ -13,5 +15,20 @@ public class ListData extends TreeMap<Key, int[]> implements Data
         if (data != null)
             this.putAll(((ListData)data));
         return this;
+    }
+
+    @Override
+    public String toString()
+    {
+        StringBuilder sb = new StringBuilder().append('{');
+        boolean first = true;
+        for (Map.Entry<Key, int[]> entry : entrySet())
+        {
+            if (!first)
+                sb.append(", ");
+            sb.append(entry.getKey()).append(" -> ").append(Arrays.toString(entry.getValue()));
+            first = false;
+        }
+        return sb.append('}').toString();
     }
 }
